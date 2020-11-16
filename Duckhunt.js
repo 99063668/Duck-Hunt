708cd6d607b3e6d2147e743d1d3a873ab22cab89
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-11-16 19:54:19 +0100

    Add files via upload

--- Duckhunt.js
+++ Duckhunt.js
@@ -1,34 +1,44 @@
 var duck = document.getElementById('duck');
-var move = document.getElementById('duck');
-var position = 0;
-
-
 duck.style.position = 'relative';
-
-move.onclick = function fly(direction){
-    position += 75;
-
-    duck.style.left = position + 'px';
-    duck.style.right = position + 'px';
-    duck.style.top = position + 'px';
-    duck.style.bottom = position + 'px';
-}
+var score = 0
+var timing = setInterval(moveduck, 5000);
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
 
 
-
-
-var object = document.getElementById('duck');
-
-object.onclick=function(){
-  var x = Math.floor(Math.random()*75);
-  var y = Math.floor(Math.random()*75);
-  var z = Math.floor(Math.random()*75);
-  var r = Math.floor(Math.random()*75);
-  object.style.top = x + 'px';
-  object.style.left = y + 'px';
-  object.style.right = z + 'px';
-  object.style.bottom = r + 'px';
+function moveduck() {
+  var top = Math.floor(Math.random() * 750);
+  var left = Math.floor(Math.random() * 750);
+  var right = Math.floor(Math.random() * 750);
+  var bottom = Math.floor(Math.random() * 750);
+  var vertical = Math.floor(Math.random() * 750);
+  var horizontal = Math.floor(Math.random() * 750);
+  duck.style.top = top + 'px';
+  duck.style.left = left + 'px';
+  duck.style.right = right + 'px';
+  duck.style.bottom = bottom + 'px';
+  duck.style.vertical = vertical + 'px';
+  duck.style.horizontal = horizontal + 'px';
+  score++
 };
-    
 
-    
+document.addEventListener("click", function(scoreboard){
+if(score === -1){
+    score = 0
+    document.getElementById('score').innerHTML = 'Game Over!'
+    alert("Game Over!")
+    console.log('Game Over!')
+  }
+if(score === 20){
+    document.getElementById('score').innerHTML = 'You Win!'
+    alert("You Win!")
+    console.log('You Win!')
+  }
+  if (scoreboard.target.id === 'duck'){
+    moveduck()
+     document.getElementById('score').innerHTML = score
+  }else {
+  score--
+  document.getElementById('score').innerHTML = score
+  }
+})
