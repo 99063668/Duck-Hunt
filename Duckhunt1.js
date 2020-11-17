eea5a7f432fadd18a115dac95e40ed119e55f179
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-11-17 11:05:07 +0100

    Add files via upload

--- Duckhunt1.js
+++ Duckhunt1.js
@@ -1,8 +1,11 @@
 var duck = document.getElementById('duck');
+var duck2 = document.getElementById('duck2');
 duck.style.position = 'relative';
+duck2.style.position = 'relative';
 var score = 0
 var timing2 = setInterval(movedog, 8000);
 var timing = setInterval(moveduck, 5000);
+var timing2 = setInterval(moveduck2, 3000);
 var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 
@@ -29,6 +32,22 @@ function moveduck() {
   score++
 };
 
+function moveduck2() {
+    var top = Math.floor(Math.random() * 750);
+    var left = Math.floor(Math.random() * 750);
+    var right = Math.floor(Math.random() * 750);
+    var bottom = Math.floor(Math.random() * 750);
+    var vertical = Math.floor(Math.random() * 750);
+    var horizontal = Math.floor(Math.random() * 750);
+    duck2.style.top = top + 'px';
+    duck2.style.left = left + 'px';
+    duck2.style.right = right + 'px';
+    duck2.style.bottom = bottom + 'px';
+    duck2.style.vertical = vertical + 'px';
+    duck2.style.horizontal = horizontal + 'px';
+    score++
+  };
+
 document.addEventListener("click", function(scoreboard){
 if(score === -1){
     score = 0
@@ -44,7 +63,13 @@ if(score === 20){
   if (scoreboard.target.id === 'duck'){
     moveduck()
      document.getElementById('score').innerHTML = score
-  }else {
+     
+  }
+  if (scoreboard.target.id === 'duck2'){
+    moveduck2()
+     document.getElementById('score').innerHTML = score
+  }
+  else {
   score--
   document.getElementById('score').innerHTML = score
   }
