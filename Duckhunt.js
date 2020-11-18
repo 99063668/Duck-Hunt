87329a8d276bb9a50b78c7ae983059735ae86d5d
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-11-18 11:51:29 +0100

    Add files via upload

--- Duckhunt.js
+++ Duckhunt.js
@@ -15,6 +15,29 @@ function movedog(){
 
 
 function moveduck() {
+  var top = Math.floor(Math.random() * Math.min(750, window.innerHeight));;
+  var left = Math.floor(Math.random() * Math.min(750, window.innerWidth));
+  var right = Math.floor(Math.random() * Math.min(750, window.innerWidth));
+  var bottom = Math.floor(Math.random() * Math.min(750, window.innerHeight));
+  duck.style.top = top + 'px';
+  duck.style.left = left + 'px';
+  duck.style.right = right + 'px';
+  duck.style.bottom = bottom + 'px';
+  score++
+};
+
+var duck = document.getElementById('duck');
+duck.style.position = 'relative';
+var score = 0
+var miss = 0
+var hit = 0
+var count = 0
+var timing = setInterval(moveduck, 5000);
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+
+
+function moveduck() {
   var top = Math.floor(Math.random() * 750);
   var left = Math.floor(Math.random() * 750);
   var right = Math.floor(Math.random() * 750);
@@ -27,25 +50,37 @@ function moveduck() {
   duck.style.bottom = bottom + 'px';
   duck.style.vertical = vertical + 'px';
   duck.style.horizontal = horizontal + 'px';
-  score++
+ 
 };
 
-document.addEventListener("click", function(scoreboard){
+function showBoard(){
+  document.getElementById('container').innerHTML =''
+  document.getElementById('container').innerHTML += `<div>shot fired: ${count}, hits/missed: ${hit}/${miss}, accuracy: ${(hit/count * 100).toFixed(2)}%, Total: ${score}</div>`
+}
+
+document.addEventListener("click", function(e){
+count++
 if(score === -1){
-    score = 0
-    document.getElementById('score').innerHTML = 'Game Over!'
-    alert("Game Over!")
-    console.log('Game Over!')
+  score = 0
+  document.getElementById('score').innerHTML = 'Game over!'
+  console.log('game over')
+  showBoard()
   }
-if(score === 20){
-    document.getElementById('score').innerHTML = 'You Win!'
-    alert("You Win!")
-    console.log('You Win!')
+
+if(score >= 20){
+  document.getElementById('score').innerHTML = 'You Win!'
+  console.log('Win')
+  showBoard()
   }
-  if (scoreboard.target.id === 'duck'){
-    moveduck()
-     document.getElementById('score').innerHTML = score
-  }else {
+
+if (e.target.id === 'duck'){
+  hit++
+  score++
+  moveduck()
+  document.getElementById('score').innerHTML = score
+
+}else {
+  miss++
   score--
   document.getElementById('score').innerHTML = score
   }
--- Duckhunt1.js
+++ Duckhunt1.js
@@ -33,44 +33,47 @@ function moveduck() {
 };
 
 function moveduck2() {
-    var top = Math.floor(Math.random() * 750);
-    var left = Math.floor(Math.random() * 750);
-    var right = Math.floor(Math.random() * 750);
-    var bottom = Math.floor(Math.random() * 750);
-    var vertical = Math.floor(Math.random() * 750);
-    var horizontal = Math.floor(Math.random() * 750);
+  var top = Math.floor(Math.random() * Math.min(750, window.innerHeight));;
+  var left = Math.floor(Math.random() * Math.min(750, window.innerWidth));
+  var right = Math.floor(Math.random() * Math.min(750, window.innerWidth));
+  var bottom = Math.floor(Math.random() * Math.min(750, window.innerHeight));
     duck2.style.top = top + 'px';
     duck2.style.left = left + 'px';
     duck2.style.right = right + 'px';
     duck2.style.bottom = bottom + 'px';
-    duck2.style.vertical = vertical + 'px';
-    duck2.style.horizontal = horizontal + 'px';
     score++
   };
 
-document.addEventListener("click", function(scoreboard){
-if(score === -1){
+  function showBoard(){
+    document.getElementById('container').innerHTML =''
+    document.getElementById('container').innerHTML += `<div>shot fired: ${count}, hits/missed: ${hit}/${miss}, accuracy: ${(hit/count * 100).toFixed(2)}%, Total: ${score}</div>`
+  }
+  
+  document.addEventListener("click", function(e){
+  count++
+  if(score === -1){
     score = 0
-    document.getElementById('score').innerHTML = 'Game Over!'
-    alert("Game Over!")
-    console.log('Game Over!')
-  }
-if(score === 20){
+    document.getElementById('score').innerHTML = 'Game over!'
+    console.log('game over')
+    showBoard()
+    }
+  
+  if(score >= 20){
     document.getElementById('score').innerHTML = 'You Win!'
-    alert("You Win!")
-    console.log('You Win!')
-  }
-  if (scoreboard.target.id === 'duck'){
+    console.log('Win')
+    showBoard()
+    }
+  
+  if (e.target.id === 'duck'){
+    hit++
+    score++
     moveduck()
-     document.getElementById('score').innerHTML = score
-     
-  }
-  if (scoreboard.target.id === 'duck2'){
-    moveduck2()
-     document.getElementById('score').innerHTML = score
-  }
-  else {
-  score--
-  document.getElementById('score').innerHTML = score
-  }
-})
+    document.getElementById('score').innerHTML = score
+  
+  }else {
+    miss++
+    score--
+    document.getElementById('score').innerHTML = score
+    }
+  })
+  
