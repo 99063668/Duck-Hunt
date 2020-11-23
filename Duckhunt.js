cc44095d8054da68d0a26dbdcbed0c46bcba2f5d
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-11-23 10:59:33 +0100

    Add files via upload

--- Duckhunt.js
+++ Duckhunt.js
@@ -3,14 +3,14 @@ duck.style.position = 'relative';
 var score = 0
 var score1 = 0
 var timing = setInterval(moveduck, 5000);
-var hit =0
-var miss =0
+var hit = 0
+var miss = 0
 
 function moveduck() {
-  var top = Math.floor(Math.random() * Math.min(750, window.innerHeight));;
-  var left = Math.floor(Math.random() * Math.min(750, window.innerWidth));
-  var right = Math.floor(Math.random() * Math.min(750, window.innerWidth));
-  var bottom = Math.floor(Math.random() * Math.min(750, window.innerHeight));
+  var top = Math.floor(Math.random() * (750, window.outerHeight));;
+  var left = Math.floor(Math.random() * (750, window.outerWidth));
+  var right = Math.floor(Math.random() * (750, window.outerWidth));
+  var bottom = Math.floor(Math.random() * (750, window.outerHeight));
   duck.style.top = top + 'px';
   duck.style.left = left + 'px';
   duck.style.right = right + 'px';
@@ -25,7 +25,6 @@ var miss = 0
 var hit = 0
 var count = 0
 var timing = setInterval(moveduck, 5000);
-
 
 function moveduck() {
   var top = Math.floor(Math.random() * 750);
